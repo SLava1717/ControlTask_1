3f50209c3c27645e30cb7d3a1891c7f837ab3127
Slava <slava.kostromin.2000@mail.ru>
2022-11-29 17:18:37 +0300

    Решил контрольную задачу

---
+++ Control_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите данные через пробел: ");
+string[] date = Console.ReadLine().Split(' ');
+string[] result = new string[date.Length];
+void ResultArray(string[] date, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < date.Length; i++)
+    {
+        if(date[i].Length <= 3)
+        {
+            result[count] = date[i];
+            count++;
+        }
+    }
+}
+void OutputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(date, result);
+OutputArray(result);
